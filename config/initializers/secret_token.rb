4fe1215ec1fd15b4b9a015e69fe35888f9ce2555
Francesco Lazzarino <flazzarino@gmail.com>
2011-03-16 21:47:13 -0400

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Irclone::Application.config.secret_token = '9261dd77219be5515f900c8a6577cf3559a55428543614140cacee5aefc3e85d0b8f4f49cccbf97c4b47cf0fab5b0980bb551db28de14417d2f9bcc7c13cec6e'
