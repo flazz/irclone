1bea711e661efdce8724482f7e3a05770de86975
Francesco Lazzarino <flazzarino@gmail.com>
2011-03-17 14:01:30 -0400

    simplify interactive session

--- features/chat.feature
+++ features/chat.feature
@@ -14,10 +14,5 @@ Feature: chat in a channel
 
   Scenario: chat with others in a channel
     Given some user "leo" says "hi there neo!" in "matrix"
-    And I fill in "message" with "hiya leo"
-    And I press "send"
-    And some user "leo" says "till next time" in "matrix"
     When I wait 1 second
     Then I should see "leo: hi there neo!" within "#log"
-    And I should see "neo: hiya leo" within "#log"
-    And I should see "leo: till next time" within "#log"
--- features/support/env.rb
+++ features/support/env.rb
@@ -15,7 +15,9 @@ require 'cucumber/web/tableish'
 require 'capybara/rails'
 require 'capybara/cucumber'
 require 'capybara/session'
-require 'cucumber/rails/capybara_javascript_emulation' # Lets you click links with onclick javascript handlers without using @culerity or @javascript
+#require 'cucumber/rails/capybara_javascript_emulation' # Lets you click links with onclick javascript handlers without using @culerity or @javascript
+Capybara.javascript_driver = :selenium
+
 # Capybara defaults to XPath selectors rather than Webrat's default of CSS3. In
 # order to ease the transition to Capybara we set the default here. If you'd
 # prefer to use XPath just remove this line and adjust any selectors in your
@@ -42,3 +44,4 @@ if defined?(ActiveRecord::Base)
   rescue LoadError => ignore_if_database_cleaner_not_present
   end
 end
+Before { $redis.flushdb }
